1f8d8a03ecca46625f4c244380ec525b12eabb5c
Luvier Vizcaino <>
2023-03-19 06:03:49 -0500

    POM: add some functions related with the home page

--- cypress/e2e/homepage/carousel.cy.js
+++ cypress/e2e/homepage/carousel.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="Cypress" />
 import home from '../../pages/home.js'
 
 describe('Mall.cz Homepage Carousels Test', () => {
@@ -7,19 +8,13 @@ describe('Mall.cz Homepage Carousels Test', () => {
   })
 
   it('should verify that each carousel on the homepage displays 15 unique elements', () => {
-    // // Get all carousels on the homepage
-    // cy.get('.carousel-selector').each(($carousel) => {
-    //   // Get the number of unique elements in the carousel
-    //   const uniqueElements = new Set($carousel.children()).size
+    // Get all carousels on the homepage
+    home.getTopIconsCarousel().each((carousel) => {
+      expect(carousel.children().length()).to.equal(15)
+    })
 
-    //   // Check if the carousel has 15 unique elements
-    //   if (uniqueElements === 15) {
-    //     cy.log(`Carousel with 15 unique elements found: ${$carousel}`)
-    //   } else {
-    //     // If the carousel does not have 15 unique elements, the test should fail
-    //     cy.log(`Carousel with ${uniqueElements} unique elements found, expected 15: ${$carousel}`)
-    //     expect(uniqueElements).to.equal(15)
-    //   }
-    // })
+    home.getAllProductsCarousel().each((carousel) => {
+      expect(carousel.children().length()).to.equal(15)
+    })
   })
 })
--- cypress/pages/home.js
+++ cypress/pages/home.js
@@ -1,7 +1,26 @@
+/// <reference types="Cypress" />
 export class home {
   static visit() {
     cy.visit('/')
   }
+
+  static getTopIconsCarousel() {
+    return cy.get('.top-icons')
+  }
+
+  static getTopIconsItemsCarouse() {
+    return cy.get('.top-icons__list > li')
+  }
+
+  static getAllProductsCarousel() {
+    return cy.get('.hooper-list')
+  }
+
+  static getAllProductsListCarousel() {
+    return cy.get('.hooper-track > li')
+  }
+
+
 }
 
 export default home
